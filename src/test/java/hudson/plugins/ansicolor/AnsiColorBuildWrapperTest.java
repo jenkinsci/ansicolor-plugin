5c724f40ca9012a451703f7a64c5c618f7f4d2dc
Jesse Glick <jglick@cloudbees.com>
2018-10-30 12:32:34 -0400

    [JENKINS-54133] Pregenerating console notes sent to agents for some common cases.

--- src/test/java/hudson/plugins/ansicolor/AnsiColorBuildWrapperTest.java
+++ src/test/java/hudson/plugins/ansicolor/AnsiColorBuildWrapperTest.java
@@ -1,11 +1,12 @@
 package hudson.plugins.ansicolor;
 
 import hudson.Functions;
+import hudson.console.ConsoleNote;
 import java.io.StringWriter;
+import java.util.logging.Level;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
-import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Assume;
@@ -13,6 +14,8 @@ import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.runners.model.Statement;
 import org.jvnet.hudson.test.BuildWatcher;
+import org.jvnet.hudson.test.Issue;
+import org.jvnet.hudson.test.LoggerRule;
 import org.jvnet.hudson.test.RestartableJenkinsRule;
 
 public class AnsiColorBuildWrapperTest {
@@ -42,7 +45,10 @@ public class AnsiColorBuildWrapperTest {
     public static BuildWatcher buildWatcher = new BuildWatcher();
     @Rule
     public RestartableJenkinsRule story = new RestartableJenkinsRule();
+    @Rule
+    public LoggerRule logging = new LoggerRule().recordPackage(ConsoleNote.class, Level.FINE);
 
+    @Issue("JENKINS-54133")
     @Test
     public void testWorkflowWrap() throws Exception {
         story.addStep(new Statement() {
@@ -50,9 +56,10 @@ public class AnsiColorBuildWrapperTest {
             @Override
             public void evaluate() throws Throwable {
                 Assume.assumeTrue(!Functions.isWindows());
+                story.j.createSlave();
                 WorkflowJob p = story.j.jenkins.createProject(WorkflowJob.class, "p");
                 p.setDefinition(new CpsFlowDefinition(
-                        "node {\n"
+                        "node('!master') {\n"
                         + "  wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm', 'defaultFg': 1, 'defaultBg': 2]) {\n"
                         + "    sh(\"\"\"#!/bin/bash\n"
                         + "      printf 'The following word is supposed to be \\\\e[31mred\\\\e[0m\\\\n'\"\"\"\n"
